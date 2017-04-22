6c366991ff4b289c628822934d09c485eddf0884
Daniel Crowson <danieljcrowson@gmail.com>
2017-04-22 17:37:51 -0500

    routing complete

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
-
+import { Router } from '@angular/router';
 
 
 @Component({
-  //selector: 'app-root',
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
@@ -15,7 +14,10 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+      private heroService: HeroService,
+      private router: Router
+    ) { }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
@@ -27,6 +29,10 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
     this.getHeroes();
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 
 }
